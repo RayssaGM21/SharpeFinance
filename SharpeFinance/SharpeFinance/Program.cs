5875c90e0f14d3d7e2d0b290454068f81f025403
maisa <maisaamaral220@gmail.com>
2025-03-27 17:55:12 -0300

    teste01

--- SharpeFinance/SharpeFinance/Program.cs
+++ SharpeFinance/SharpeFinance/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-/*teste  2*/
+﻿using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    private static List<conta> contas = new List<conta>();
+    public static void CadastrarConta()
+    {
+        Console.WriteLine("Digite o nome do seu banco:");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite o seu saldo atual:");
+        string saldo = Console.ReadLine();
+
+        List<conta> saldo_total = new List<conta>();
+        contas.Add(new conta(nome, saldo_total));
+
+        Console.WriteLine("Sua Conta foi cadastrada!");
+
+        for conta in contas
+        {
+            Console.WriteLine(CadastrarConta[0], conta[1]);
+        }
+
+        Console.WriteLine($nome{ nome}
+        { saldo_total}
+        ");
+    }
+    public static void Main(string[] args)
+    {
+        CadastrarConta();
+    }
+}
