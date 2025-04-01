4c75c9c4c67e450e66358303ca86aa16d60219a0
samara <adornosamara165@gmail.com>
2025-03-31 21:01:11 -0300

    aula 31 de março

--- SharpeFinance/SharpeFinance/Program.cs
+++ SharpeFinance/SharpeFinance/Program.cs
@@ -2,24 +2,61 @@
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static void exibeContas(List<List<(string, decimal)>> contas)
     {
-        List<string> contas = new List<string>();
-
-        Console.WriteLine("Digite o nome da sua conta:");
-        string nome = Console.ReadLine();
-
-        Console.WriteLine("Digite o saldo da sua conta:");
-        string saldo = Console.ReadLine();
-
-        contas.Add($"Nome: {nome} Saldo: {saldo}");
-
-        Console.WriteLine("Sua conta foi cadastrada!");
-        Console.WriteLine("Teste");
-
+        int contador = 0;
         foreach (var conta in contas)
         {
-            Console.WriteLine(conta);
+            foreach (var item in conta)
+            {
+                Console.WriteLine($"{contador}) {item}");
+                contador++;
+            }
+        }
+    }
+    public static void Main(string[] args)
+    {
+        /*
+            [[nubank, 235.70], [banco do brasil, 1300], [bradesco, 45]]
+        */
+
+        List<List<(string, decimal)>> contas = new List<List<(string, decimal)>>();
+        Boolean x = true;
+        while (x == true) { 
+            Console.WriteLine("Digite o nome da sua conta:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o saldo da sua conta:");
+            decimal saldo = Convert.ToDecimal(Console.ReadLine());
+
+            contas.Add([(nome, saldo)]);
+
+            Console.WriteLine("Sua conta foi cadastrada com sucesso!");
+            Console.WriteLine("Deseja continuar cadastrando uma conta (SIM ou NAO)?");
+            string opcao = Console.ReadLine();
+            if (opcao == "SIM")
+            {
+                x = true;
+            }
+            else
+            {
+                x = false;
+            }
+
+        }
+        
+
+        Console.WriteLine("Qual tipo de transação você deseja realizar (Receita ou Despesa):");
+        string tipo_transacao = Console.ReadLine();
+
+        Console.WriteLine("Digite o valor da transação:");
+        decimal valor_transacao = Convert.ToDecimal(Console.ReadLine());
+
+        exibeContas(contas);
+
+        if (tipo_transacao == "Receita")
+        {
+
         }
     }
 }
